13ad27ce0a0617e44e3638f69440ef1c8f36c3fe
Emanuel Rojas <emarojas710@gmail.com>
2026-06-10 22:05:42 -0300

    se agrega icono

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
+
 const app = createApp(App)
 
 app.component('Icon', Icon)
